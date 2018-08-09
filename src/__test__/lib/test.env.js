a4525fafb0cf95d74fabdd7dfd8e9a30dda33fb3
Tracy Williams <selpilot@gmail.com>
2018-08-08 17:01:37 -0700

    got tests for auth and profile working

--- src/__test__/lib/test.env.js
+++ src/__test__/lib/test.env.js
@@ -7,14 +7,6 @@ process.env.SECRET = 'Hu;asdfiwknlasgfnwkeo002222njaksdfsdoclxo89834ht25585552';
 
 // set this to true or false depending on if you want to hit the mock AWS-SDK 
 // set to false if you want to make a real API call to your bucket
-const isAwsMock = true;
 
-if (isAwsMock) {
-  process.env.AWS_BUCKET = 'fake';
-  process.env.AWS_SECRET_ACCESS_KEY = 'fake';
-  process.env.AWS_ACCESSS_KEY_ID = 'fake';
-  require('./setup');
-} else {
-  // remember to set your .env vars and add .env in .gitignore
-  require('dotenv').config();
-}
+// remember to set your .env vars and add .env in .gitignore
+require('dotenv').config();
