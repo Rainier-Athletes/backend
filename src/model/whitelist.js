a35c2f90412bdbebd1ddc4bba74f8c8959bb0d2e
Kevin Hwangpo <hwangpo3@gmail.com>
2018-08-09 13:24:06 -0700

    added in more properties on schema

--- src/model/whitelist.js
+++ src/model/whitelist.js
@@ -4,7 +4,23 @@ const whitelistSchema = mongoose.Schema({
   email: {
     type: String, 
     required: true,
-  }
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['admin', 'mentor'],
+    default: 'mentor',
+  },
+  accountId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    unique: true,
+  },
+  profileId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    unique: true,
+  },
 });
 
 const skipInit = process.env.NODE_ENV === 'development';
--- src/router/whitelist-router.js
+++ src/router/whitelist-router.js
@@ -3,27 +3,26 @@ import HttpErrors from 'http-errors';
 import Whitelist from '../model/whitelist';
 import bearerAuthMiddleware from '../lib/middleware/bearer-auth-middleware';
 import logger from '../lib/logger';
-import whitelist from '../model/whitelist';
 
-const whitelistRouter = new Router(); 
+const whitelistRouter = new Router();
 
 whitelistRouter.post('/api/v1/whitelists',
-bearerAuthMiddleware, (request, response, next) => {
-  logger.log(logger.INFO, `.post /api/whitelists req.body: ${request.body}`);
-  Whitelist.init()
-    .then(() => {
-      return new Whitelist({
-        ...request.body,
-        accountId: request.account._id, 
-      }).save();
-    })
-    .then((whitelist) => {
-      logger.log(logger.INFO, `POST WHITELIST ROUTER: new email authorization with 200 code, ${JSON.stringify(whitelist)}`);
-      return response.json(whitelist);
-    })
-    .catch(next);
+  bearerAuthMiddleware, (request, response, next) => {
+    logger.log(logger.INFO, `.post /api/whitelists req.body: ${request.body}`);
+    Whitelist.init()
+      .then(() => {
+        return new Whitelist({
+          ...request.body,
+          accountId: request.account._id,
+        }).save();
+      })
+      .then((whitelist) => {
+        logger.log(logger.INFO, `POST WHITELIST ROUTER: new email authorization with 200 code, ${JSON.stringify(whitelist)}`);
+        return response.json(whitelist);
+      })
+      .catch(next);
     return undefined;
-});
+  });
 
 whitelistRouter.get(['/api/v1/whitelists', '/api/whitelists/me'], bearerAuthMiddleware, (request, response, next) => {
   if (!request.whitelist) return next(new HttpErrors(404, 'WHITELIST ROUTER GET: whitelist not found. Missing email.', { expose: false }));
@@ -34,10 +33,10 @@ whitelistRouter.get(['/api/v1/whitelists', '/api/whitelists/me'], bearerAuthMidd
         .then((whitelist) => {
           return response.json(whitelist);
         });
-        return undefined;
+      return undefined;
     })
     .catch(next);
-    return undefined;
+  return undefined;
 });
 
 whitelistRouter.put('/api/v1/whitelists', bearerAuthMiddleware, (request, response, next) => {
@@ -56,7 +55,7 @@ whitelistRouter.put('/api/v1/whitelists', bearerAuthMiddleware, (request, respon
       response.json(whitelist);
     })
     .catch(next);
-    return undefined
+  return undefined;
 });
 
 whitelistRouter.delete('/api/v1/whitelists', bearerAuthMiddleware, (request, response, next) => {
@@ -66,4 +65,6 @@ whitelistRouter.delete('/api/v1/whitelists', bearerAuthMiddleware, (request, res
     .then(() => {
       return Whitelist.findByIdAndRemove(request.query.id);
     })
-})
+    .catch(next);
+  return undefined;
+});
