af9379fbb0bb1214481ad2e297485ff142c1075e
Tracy Williams <selpilot@gmail.com>
2018-08-13 16:01:04 -0700

    trying to get tests working. need a big merge first

--- src/lib/server.js
+++ src/lib/server.js
@@ -11,7 +11,6 @@ import loggerMiddleware from './middleware/logger-middleware';
 // our routes
 import authRouter from '../router/auth-router';
 import googleOauthRouter from '../router/google-oauth-router';
-import basecampOauthRouter from '../router/basecamp-oauth-router';
 import profileRouter from '../router/profile-router';
 import pointTrackerRouter from '../router/point-tracker-router';
 import whitelistRouter from '../router/whitelist-router';
@@ -20,40 +19,6 @@ import relationshipRouter from '../router/relationship-router';
 const app = express();
 const PORT = process.env.PORT || 3000;
 let server = null;
-
-// cors options needed for use with front-end lab 36-40
-// const corsOptions = { 
-//   origin: 'http://localhost:8080',
-//   credentials: true,
-// };
-// const corsOptions = {
-//   // origin: process.env.CORS_ORIGINS,
-//   // "origin" defines what front end domains are permitted to access our API, we need to implement this to prevent any potential attacks
-//   origin: (origin, cb) => {
-//     console.log('server origin:', origin);
-//     if (!origin) {
-//       // assume Google API or Cypress
-//       cb(null, true);
-//     // } else if (process.env.CORS_ORIGINS.includes(origin)) {
-//     } else if (origin.includes(process.env.CORS_ORIGINS)) {
-//       console.log('server origin accepted by .includes code');
-//       cb(null, true);
-//     } else {
-//       cb(new Error(`${origin} not allowed by CORS`));
-//     }
-//   },
-//   credentials: true, // Configures the Access-Control-Allow-Credentials CORS header. Set to true to pass the header, otherwise it is omitted.
-// };
-// third party apps
-// app.use(cors(corsOptions));
-
-// cors middleware function from https://jonathanmh.com/how-to-enable-cors-in-express-js-node-js/
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
-// breaks localhost connection
 
 // here's the cors docs implementation:
 // const whitelist = ['http://localhost:8080', 'http://mygarage-frontend.herokuapp.com'];
@@ -86,7 +51,6 @@ app.use(express.json());
 app.use(loggerMiddleware);
 app.use(authRouter);
 app.use(googleOauthRouter);
-app.use(basecampOauthRouter);
 app.use(whitelistRouter);
 app.use(profileRouter);
 app.use(pointTrackerRouter);
--- src/model/profile.js
+++ src/model/profile.js
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose';
-import logger from '../lib/logger';
 
 const profileSchema = mongoose.Schema({
   active: {
