ee74a05bebe5874a65b9dd88dce26afc17832004
Tracy Williams <selpilot@gmail.com>
2018-08-16 19:09:35 -0700

    removed synopsis test

--- src/__test__/point-tracker.test.js
+++ src/__test__/point-tracker.test.js
@@ -2,20 +2,22 @@ import superagent from 'superagent';
 import bearerAuth from 'superagent-auth-bearer';
 import { createPointTrackerMockPromise, removeAllResources } from './lib/point-tracker-mock';
 import PointTracker from '../model/point-tracker';
-import { startServer } from '../lib/server';
+import { startServer, stopServer } from '../lib/server';
 
 bearerAuth(superagent);
 
 const apiUrl = `http://localhost:${process.env.PORT}/api/v1`;
 
 describe('TESTING POINT-TRACKER ROUTER', () => {
-  beforeAll(async () => { await startServer(); });
 
   let mockData;
   beforeEach(async () => {
+    await startServer();
     await removeAllResources();
     mockData = await createPointTrackerMockPromise();
   });
+
+  afterEach(async () => { stopServer(); });
 
   describe('Testing point-tracker POST route', () => {
     test('POST 200 good request', async () => {
