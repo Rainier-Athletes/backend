91b7e63beff586320d25d50af5b691db2ef31ad0
Tracy Williams <selpilot@gmail.com>
2018-09-12 15:16:52 -0700

    point-tracker tests passing after refactoring

--- src/router/point-tracker-router.js
+++ src/router/point-tracker-router.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import HttpErrors from 'http-errors';
 import PointTracker from '../model/point-tracker';
-import Profile from '../model/profile';
+import StudentData from '../model/student-data';
 import bearerAuthMiddleware from '../lib/middleware/bearer-auth-middleware';
 
 const pointTrackerRouter = new Router();
@@ -39,10 +39,10 @@ pointTrackerRouter.post('/api/v1/pointstracker', bearerAuthMiddleware, (request,
   PointTracker.init()
     .then(() => {
       // get student's data from mongo
-      return Profile.findById(request.body.student);
+      return StudentData.findOne({ student: request.body.student });
     })
-    .then((student) => {
-      if (!student) return next(new HttpErrors(400, 'POINT-TRACKER ROUTER POST: Missing student id in req body', { expose: false }));
+    .then((studentData) => {
+      if (!studentData) return next(new HttpErrors(400, 'POINT-TRACKER ROUTER POST: Missing student id in req body', { expose: false }));
       
       // if this point tracker is being submitted by a substitute, take the submitter's
       // id off the request.profile (via bearer auth) and add it to the point tracker
@@ -50,8 +50,8 @@ pointTrackerRouter.post('/api/v1/pointstracker', bearerAuthMiddleware, (request,
         request.body.mentor = request.profile._id.toString();
       } else {
         // submitter isn't a sub. Get mentor ID from student's profile
-        const [mentor] = student.studentData.mentors.filter(m => m.currentMentor);
-        request.body.mentor = mentor.id._id.toString(); // findById autopopulates so id is the mentor, not just id.
+        const [mentors] = studentData.mentors.filter(m => m.currentMentor);
+        request.body.mentor = mentors.mentor._id.toString(); // findById autopopulates so id is the mentor, not just id.
       }
       return new PointTracker(request.body).save();
     })
