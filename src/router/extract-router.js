d79ce1ea8142273b95148b1d6294f3c524c175e1
Tracy Williams <selpilot@gmail.com>
2018-09-13 11:21:29 -0700

    refactored csv building to use streams

--- src/router/extract-router.js
+++ src/router/extract-router.js
@@ -8,7 +8,6 @@ import { Router } from 'express';
 import { google } from 'googleapis';
 import HttpError from 'http-errors';
 import fs from 'fs';
-import CsvBuilder from 'csv-builder';
 
 import bearerAuthMiddleware from '../lib/middleware/bearer-auth-middleware';
 import logger from '../lib/logger';
@@ -156,169 +155,23 @@ extractRouter.get('/api/v1/extract', bearerAuthMiddleware, async (request, respo
     return uploadFileToFolder(folderId);
   } // end of sendFileToGoogleDrive
   
-  const headers = ['date',
-    'student.active',
-    'student.firstName',
-    'student.lastName',
-    'student.email',
-    'student.phone',
-    'student.gender',
-    'student.school',
-    'mentorIsSubstitute',
-    'mentor.firstName',
-    'mentor.lastName',
-    'surveyQuestions.mentorAttendedCheckin',
-    'surveyQuestions.metFaceToFace', 
-    'surveyQuestions.hadOtherCommunication',
-    'surveyQuestions.hadNoCommunication',
-    'surveyQuestions.scoreSheetTurnedIn',
-    'surveyQuestions.scoreSheetLostOrIncomplete',
-    'surveyQuestions.scoreSheetWillBeLate',
-    'surveyQuestions.scoreSheetOther',
-    'surveyQuestions.scoreSheetOtherReason',
-    'surveyQuestions.synopsisInformationComplete',
-    'surveyQuestions.synopsisInformationIncomplete',
-    'surveyQuestions.synopsisCompletedByRaStaff',
-    'synopsisComments.extraPlayingTime',
-    'synopsisComments.mentorGrantedPlayingTime',
-    'synopsisComments.studentActionItems',
-    'synopsisComments.sportsUpdate',
-    'synopsisComments.additionalComments',
-    'subject.1',
-    'subject.1.excusedDays',
-    'subject.1.stamps',
-    'subject.1.halfStamp',
-    'subjects.1.tutorials',
-    'subjects.1.grade',
-    'subject.2',
-    'subject.2.excusedDays',
-    'subject.2.stamps',
-    'subject.2.halfStamp',
-    'subjects.2.tutorials',
-    'subjects.2.grade',  
-    'subject.3',
-    'subject.3.excusedDays',
-    'subject.3.stamps',
-    'subject.3.halfStamp',
-    'subjects.3.tutorials',
-    'subjects.3.grade',  
-    'subject.4',
-    'subject.4.excusedDays',
-    'subject.4.stamps',
-    'subject.4.halfStamp',
-    'subjects.4.tutorials',
-    'subjects.4.grade',  
-    'subject.5',
-    'subject.5.excusedDays',
-    'subject.5.stamps',
-    'subject.5.halfStamp',
-    'subjects.5.tutorials',
-    'subjects.5.grade',  
-    'subject.6',
-    'subject.6.excusedDays',
-    'subject.6.stamps',
-    'subject.6.halfStamp',
-    'subjects.6.tutorials',
-    'subjects.6.grade',  
-    'subject.7',
-    'subject.7.excusedDays',
-    'subject.7.stamps',
-    'subject.7.halfStamp',
-    'subjects.7.tutorials',
-    'subjects.7.grade',
-    'subject.8',
-    'subject.8.excusedDays',
-    'subject.8.stamps',
-    'subject.8.halfStamp',
-    'subjects.8.tutorials',
-    'subjects.8.grade',
-  ];
-
-  const alias = {
-    'subject.1': 'subjects[0].subjectName',
-    'subject.1.excusedDays': 'subjects[0].scoring.excusedDays',
-    'subject.1.stamps': 'subjects[0].scoring.stamps',
-    'subject.1.halfStamp': 'subjects[0].scoring.halfStamp',
-    'subject.1.tutorials': 'subjects[0].scoring.tutorials',
-    'subject.1.grade': 'subjects[0].grade',
-    'subject.2': 'subjects[1].subjectName',
-    'subject.2.excusedDays': 'subjects[1].scoring.excusedDays',
-    'subject.2.stamps': 'subjects[1].scoring.stamps',
-    'subject.2.halfStamp': 'subjects[1].scoring.halfStamp',
-    'subject.2.tutorials': 'subjects[1].scoring.tutorials',
-    'subject.2.grade': 'subjects[1].grade',
-    'subject.3': 'subjects[2].subjectName',
-    'subject.3.excusedDays': 'subjects[2].scoring.excusedDays',
-    'subject.3.stamps': 'subjects[2].scoring.stamps',
-    'subject.3.halfStamp': 'subjects[2].scoring.halfStamp',
-    'subject.3.tutorials': 'subjects[2].scoring.tutorials',
-    'subject.3.grade': 'subjects[2].grade',
-    'subject.4': 'subjects[3].subjectName',
-    'subject.4.excusedDays': 'subjects[3].scoring.excusedDays',
-    'subject.4.stamps': 'subjects[3].scoring.stamps',
-    'subject.4.halfStamp': 'subjects[3].scoring.halfStamp',
-    'subject.4.tutorials': 'subjects[3].scoring.tutorials',
-    'subject.4.grade': 'subjects[3].grade',
-    'subject.5': 'subjects[4].subjectName',
-    'subject.5.excusedDays': 'subjects[4].scoring.excusedDays',
-    'subject.5.stamps': 'subjects[4].scoring.stamps',
-    'subject.5.halfStamp': 'subjects[4].scoring.halfStamp',
-    'subject.5.tutorials': 'subjects[4].scoring.tutorials',
-    'subject.5.grade': 'subjects[4].grade',
-    'subject.6': 'subjects[5].subjectName',
-    'subject.6.excusedDays': 'subjects[5].scoring.excusedDays',
-    'subject.6.stamps': 'subjects[5].scoring.stamps',
-    'subject.6.halfStamp': 'subjects[5].scoring.halfStamp',
-    'subject.6.tutorials': 'subjects[5].scoring.tutorials',
-    'subject.6.grade': 'subjects[5].grade',
-    'subject.7': 'subjects[6].subjectName',
-    'subject.7.excusedDays': 'subjects[6].scoring.excusedDays',
-    'subject.7.stamps': 'subjects[6].scoring.stamps',
-    'subject.7.halfStamp': 'subjects[6].scoring.halfStamp',
-    'subject.7.tutorials': 'subjects[6].scoring.tutorials',
-    'subject.7.grade': 'subjects[6].grade',
-    'subject.8': 'subjects[7].subjectName',
-    'subject.8.excusedDays': 'subjects[7].scoring.excusedDays',
-    'subject.8.stamps': 'subjects[7].scoring.stamps',
-    'subject.8.halfStamp': 'subjects[7].scoring.halfStamp',
-    'subject.8.tutorials': 'subjects[7].scoring.tutorials',
-    'subject.8.grade': 'subjects[7].grade',
-  };
-
-  const builder = new CsvBuilder({ headers, alias });
-
   // query the database and dump results to temp csv file
-  PointTracker.where('createdAt').gte(new Date(fromDate)).lte(new Date(toDate))
-  // PointTracker.where('createdAt').gte(fromDate).lte(toDate)
+  let queryError = false;
+  PointTracker.where('createdAt').gte(new Date(fromDate)).lte(new Date(toDate)).exec()
     .then((data) => {
-      if (data.length === 0) return next(HttpError(404, 'No data found in specified range', { expose: false }));
-
-      let csv = '';
-      csv += builder.getHeaders();
-      try {
-        data.forEach((item) => {
-          csv += builder.getRow(item);
-        });
-      } catch (err) {
-        return next(new HttpError(500, `Error building csv string: ${err}`));
+      if (data.length === 0) {
+        queryError = true;
+        return next(new HttpError(404, `No data found in date range ${fromDate} to ${toDate}`, { expose: false }));
       }
-
-      fs.open(`${TEMP_DIR}/${extractName}.csv`, 'wx', (oerr, fd) => {
-        if (oerr) return next(HttpError(500, `Could not create temp csv file: ${oerr}`, { expose: false }));
-        return fs.writeFile(fd, csv, (werr) => {
-          if (werr) return next(HttpError(500, `Error writing to temp csv file: ${werr}`, { expose: false }));
-          return fs.close(fd, (cerr) => {
-            if (cerr) return next(HttpError(500, `Error closing temp csv file: ${cerr}`, { expose: false }));
-            // push csv file to google drive and send response
-            return sendFileToGoogleDrive(); 
-          });
-        });
-      });
+      return PointTracker.csvReadStream(data)
+        .pipe(fs.createWriteStream(`${TEMP_DIR}/${extractName}.csv`));
+    })
+    .then(() => {
+      if (!queryError) return sendFileToGoogleDrive();
       return undefined;
     })
-    .catch((err) => {
-      return next(new HttpError(500, `Unknown server error: ${err}`));
-    });
+    .catch(next);
+
   return undefined;
 });
   
