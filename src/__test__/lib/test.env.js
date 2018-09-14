aa83a29c041cfa0bdf9d300b0b58f4d0798fe219
Tracy Williams <selpilot@gmail.com>
2018-09-14 15:34:41 -0700

    updated readme

--- src/__test__/lib/test.env.js
+++ src/__test__/lib/test.env.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = 'development';
 process.env.PORT = 5000;
-// process.env.MONGODB_URI = 'mongodb://localhost:27017/testing';
-process.env.MONGODB_URI = 'mongodb://localhost:27017/rainierathletes';
+process.env.MONGODB_URI = 'mongodb://localhost:27017/testing';
+// process.env.MONGODB_URI = 'mongodb://localhost:27017/rainierathletes';
 process.env.SECRET = 'Hu;asdfiwknlasgfnwkeo002222njaksdfsdoclxo89834ht25585552';
 process.env.CORS_ORIGINS = JSON.stringify(['http://localhost:8080']);
 // process.env.API_URL = 'http://api.rainierathletes.org/api/v1';
