5f0f944ff03909de809a2d3437b5e0127120aa4b
Karen <karenbtlai@gmail.com>
2018-10-22 15:19:06 -0400

    html pdf options

--- src/router/synopsis-router.js
+++ src/router/synopsis-router.js
@@ -9,11 +9,12 @@ import createGoogleDriveFunction from '../lib/googleDriveLib';
 const synopsisRouter = new Router();
 
 synopsisRouter.post('/api/v1/synopsis', bearerAuthMiddleware, async (request, response, next) => {
+  console.log(request.body);
   const name = typeof request.body.name === 'string' && request.body.name !== '' ? request.body.name : false;
   let title = typeof request.body.title === 'string' && request.body.title !== '' ? request.body.title : false;
   const html = typeof request.body.html === 'string' && request.body.html !== '' ? request.body.html : false;
   if (!(name && html && title)) return next(new HttpError(400, 'Missing or invalid name, title or html parameters on request body', { expose: false }));
- 
+
   title += '.pdf';
   const { googleTokenResponse } = request;
   const setFolderName = `RA Reports for ${name}`;
@@ -29,19 +30,27 @@ synopsisRouter.post('/api/v1/synopsis', bearerAuthMiddleware, async (request, re
   // has a token. sends the whole object to setCreds
   oAuth2Client.setCredentials(googleTokenResponse);
 
-  const drive = google.drive({ version: 'v3', auth: oAuth2Client }); 
-  
+  const drive = google.drive({ version: 'v3', auth: oAuth2Client });
+
   const sendFileToGoogleDrive = createGoogleDriveFunction(drive, TEMP_FILE, title, setFolderName, response, next);
 
   const options = {
     border: {
-      top: '0.5in',
-      right: '0.5in',
-      bottom: '0.5in',
+      top: '0.8in',
+      right: '0.8in',
       left: '0.5in',
     },
+    format: 'Letter',
+    orientation: 'portrait',
+    paginationOffset: 1,
+    footer: {
+      height: '28mm',
+      contents: {
+        default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+      },
+    },
   };
-  
+
   pdf.create(html, options).toFile(TEMP_FILE,
     (err) => {
       if (err) return next(new HttpError(500, 'Error creating pdf from html', { expose: false }));
@@ -50,5 +59,5 @@ synopsisRouter.post('/api/v1/synopsis', bearerAuthMiddleware, async (request, re
 
   return undefined; // to satisfy linter...
 });
-  
+
 export default synopsisRouter;
